f7fa0081a55e50c3ade706a7341fa43a4c7a9d39
chiomavera <you@example.com>
2022-06-22 13:34:35 +0100

    html file created

---
+++ index.js
@@ -0,0 +1,12 @@
+function myDetails(){
+    let name = "Nkanmuo Vera Chioma";
+    let height = "155cm";
+    let country = "Nigeria";
+    
+    // alert('my name is '+ name+', i am ' +height+' tall and i am from '+country)
+    // alert(`My name is ${name}, I am ${height} tall and I am from ${country}.`)
+
+    document.write(`My name is ${name}, I am ${height} tall and I am from ${country}.`)
+}
+
+myDetails();
